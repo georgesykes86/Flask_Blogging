10315d6c33ed7ac44099b717108aae71edd37d20
George Sykes <georgesykes86@gmail.com>
2018-08-11 13:09:18 +0100

    Add login and logout

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from flask import current_app, request, url_for
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -12,11 +17,12 @@ class Role(db.Model):
 
     users = db.relationship('User', backref='role')
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
