b455979e466efac9af2d9bd471cb4a249ba5344c
George Sykes <georgesykes86@gmail.com>
2018-08-13 11:58:22 +0100

    Add tests for updating gravatar and administrator permissions

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -4,12 +4,15 @@ from app.models import User, Role, Permission, AnonymousUser
 
 class UserModelTestCase(unittest.TestCase):
     def setUp(self):
-        self.userrole = Role(name="User")
-        self.userrole.permissions = Permission.FOLLOW | \
+        self.user_role = Role(name="User")
+        self.user_role.permissions = Permission.FOLLOW | \
                                     Permission.COMMENT | \
                                     Permission.WRITE_POSTS
-        self.user = User(email='testuser@example.com', password='password', role=self.userrole)
-        self.otherUser = User(email='othertestuser@example.com', password='password', role=self.userrole)
+        self.admin_user_role = Role(name="Admin")
+        self.admin_user_role.permissions = 0xff
+        self.user = User(email='testuser@example.com', password='password', role=self.user_role)
+        self.other_user = User(email='othertestuser@example.com', password='password', role=self.user_role)
+        self.admin_user = User(email='admin@example.com', password='password', role=self.admin_user_role)
 
     def test_password_setter(self):
         self.assertTrue(self.user.password_hash is not None)
@@ -25,7 +28,7 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(self.user.verify_password('notpassword'))
 
     def test_random_password_salting(self):
-        self.assertTrue(self.user.password_hash != self.otherUser.password_hash)
+        self.assertTrue(self.user.password_hash != self.other_user.password_hash)
 
     def test_roles_and_permissions(self):
         self.assertTrue(self.user.can(Permission.WRITE_POSTS))
@@ -34,3 +37,17 @@ class UserModelTestCase(unittest.TestCase):
     def test_anonymous_user(self):
         user = AnonymousUser()
         self.assertFalse(user.can(Permission.FOLLOW))
+
+    def test_admin_user(self):
+        self.assertTrue(self.admin_user.is_administrator())
+
+    def test_update_gravatar_hash_no_email_change(self):
+        current_hash = self.user.avatar_hash
+        self.user.update_gravatar_hash()
+        self.assertTrue(self.user.avatar_hash == current_hash)
+
+    def test_update_gravatar_hash_email_change(self):
+        current_hash = self.user.avatar_hash
+        self.user.email = 'new_email@test.com'
+        self.user.update_gravatar_hash()
+        self.assertTrue(self.user.avatar_hash != current_hash)
